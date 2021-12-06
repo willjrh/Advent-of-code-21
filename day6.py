3bc1a86c91860bb4c258589c3142ad66c1c6752e
Will <w.j.r.hewson@gmail.com>
2021-12-06 09:13:02 +0000

    Minor change to day 6

--- day6.py
+++ day6.py
@@ -1,29 +1,34 @@
-import os
+from typing import List
 import numpy as np
 
-input_file = os.path.join("inputs", "day_6.txt")
+input_file = "inputs/day_6.txt"
 
 with open(input_file) as f:
     data_in = [int(i) for i in f.readlines()[0].split(",")]
 
 
-def init_count(inital_fish):
+def init_count(inital_fish: List[int]):
     counts = []
     for i in range(7):
         counts.append(inital_fish.count(i))
     return counts
 
 
-def main():
+def main(days: int):
     fish = init_count(data_in)
     incubator = [0, 0, 0]
-    for idx in range(256):
+    for idx in range(days):
         n_new_fish = fish[np.mod(idx, 7)]
         incubator.append(n_new_fish)
         fish[np.mod(idx - 1, 7)] = fish[np.mod(idx - 1, 7)] + incubator.pop(0)
 
-    print(f"Number of fishes: {sum(fish) + sum(incubator)}")
+    return sum(fish) + sum(incubator)
 
 
 if __name__ == "__main__":
-    main()
+    n_fish = main(80)
+    # part 1
+    print(f"Number of fishes: {n_fish}")
+    n_fish = main(256)
+    # part 2
+    print(f"Number of fishes: {n_fish}")
