250e7df762d34cb2d8efac88146cc92ac4397027
Will <w.j.r.hewson@gmail.com>
2021-12-06 08:55:44 +0000

    Add day 6

---
+++ day6.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+
+sample = [3, 4, 3, 1, 2]
+input_file = os.path.join("inputs", "day_6.txt")
+
+with open(input_file) as f:
+    data_in = [int(i) for i in f.readlines()[0].split(",")]
+
+
+def init_count(inital_fish):
+    counts = []
+    for i in range(7):
+        counts.append(inital_fish.count(i))
+    return counts
+
+
+def main():
+    fish = init_count(data_in)
+    incubator = [0, 0, 0]
+    for idx in range(256):
+        new_idx = np.mod(idx, 7)
+        n_new_fish = fish[new_idx]
+        incubator.append(n_new_fish)
+        fish[np.mod(idx - 1, 7)] = fish[np.mod(idx - 1, 7)] + incubator.pop(0)
+
+    print(f"Number of fishes: {sum(fish) + sum(incubator)}")
+
+
+if __name__ == "__main__":
+    main()
